663d9352991cd49cea69ed84b622734908c9d571
Angel Aguirre <angelaguirrel@hotmail.com>
2021-03-26 15:46:17 -0600

    Added random wallpaper functionality at startup

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
-nitrogen --restore &
+
+# Get every wallpaper name in a list and get a random number
+# to select a random wallpaper from the list
+WALLPAPERS=$HOME"/Pictures/wallpapers"
+ALIST=( `ls -w1 $WALLPAPERS` )
+RANGE=${#ALIST[@]}
+let "number = $RANDOM % $RANGE"
+
+nitrogen --set-centered --save $WALLPAPERS/${ALIST[$number]}
 brightnessctl set 100% &
 cbatticon -u 5 &
 picom &
